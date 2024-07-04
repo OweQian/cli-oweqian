63a80b6c92ed3fdb337547cb21edc0c83c206613
qian wang <1510106069@qq.com>
2024-07-04 19:42:28 +0800

    feat: node最低版本检查功能

--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -1,9 +1,33 @@
 const commander = require("commander");
+const semver = require("semver");
 const createInitCommand = require("@oweqian/init");
-const { log } = require("@oweqian/utils");
+const { log, isDebug } = require("@oweqian/utils");
 
 const { program } = commander;
 const pkg = require("../package.json");
+
+const LOWEST_NODE_VERSION = "14.0.0";
+
+function checkNodeVersion() {
+  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
+    throw new Error(
+      `cli-oweqian 需要安装 ${LOWEST_NODE_VERSION} 以上版本的 Node.js`
+    );
+  }
+}
+
+function preAction() {
+  // 检查Node版本
+  checkNodeVersion();
+}
+
+process.on("uncaughtException", (e) => {
+  if (isDebug()) {
+    console.log(e);
+  } else {
+    console.log(e.message);
+  }
+});
 
 module.exports = function (args) {
   log.info("version", pkg.version);
@@ -11,7 +35,8 @@ module.exports = function (args) {
     .name(Object.keys(pkg.bin)[0])
     .usage("<command> [options]")
     .version(pkg.version)
-    .option("-d, --debug", "是否开启调试模式", false);
+    .option("-d, --debug", "是否开启调试模式", false)
+    .hook("preAction", preAction);
 
   // program
   //   .command("init [name]")
