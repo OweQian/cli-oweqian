5b03ef94d1d7fa28df488aa183d5f17724a0ba90
qian wang <1510106069@qq.com>
2024-07-10 17:34:32 +0800

    feat: cli异常监听

---
+++ packages/cli/lib/createCLI.js
@@ -0,0 +1,49 @@
+import path from "node:path";
+import { program } from "commander";
+import { dirname } from "dirname-filename-esm";
+import semver from "semver";
+import chalk from "chalk";
+import fse from "fs-extra";
+import { log } from "@oweqian/utils";
+
+const __dirname = dirname(import.meta);
+const pkgPath = path.resolve(__dirname, "../package.json");
+const pkg = fse.readJSONSync(pkgPath);
+
+const LOWEST_NODE_VERSION = "14.0.0";
+
+function checkNodeVersion() {
+  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
+    throw new Error(
+      chalk.red(
+        `cli-oweqian 需要安装 ${LOWEST_NODE_VERSION} 以上版本的 Node.js`
+      )
+    );
+  }
+}
+
+function preAction() {
+  // 检查Node版本
+  checkNodeVersion();
+}
+
+export default function createCLI() {
+  log.info("version", pkg.version);
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage("<command> [options]")
+    .version(pkg.version)
+    .option("-d, --debug", "是否开启调试模式", false)
+    .hook("preAction", preAction);
+
+  program.on("option:debug", function () {
+    if (program.opts().debug) {
+      log.verbose("debug", "launch debug mode!");
+    }
+  });
+
+  program.on("command:*", function (obj) {
+    log.error("未知的命令：" + obj[0]);
+  });
+  return program;
+}
--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -1,60 +1,9 @@
-import path from "node:path";
-import { program } from "commander";
-import semver from "semver";
-import chalk from "chalk";
-import fse from "fs-extra";
-
 import createInitCommand from "@oweqian/init";
-import { log, isDebug } from "@oweqian/utils";
-import { dirname } from "dirname-filename-esm";
-
-const __dirname = dirname(import.meta);
-const pkgPath = path.resolve(__dirname, "../package.json");
-const pkg = fse.readJSONSync(pkgPath);
-
-const LOWEST_NODE_VERSION = "14.0.0";
-
-function checkNodeVersion() {
-  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
-    throw new Error(
-      chalk.red(
-        `cli-oweqian 需要安装 ${LOWEST_NODE_VERSION} 以上版本的 Node.js`
-      )
-    );
-  }
-}
-
-function preAction() {
-  // 检查Node版本
-  checkNodeVersion();
-}
-
-process.on("uncaughtException", (e) => {
-  if (isDebug()) {
-    console.log(e);
-  } else {
-    console.log(e.message);
-  }
-});
+import createCLI from "./createCLI.js";
+import "./exception.js";
 
 export default function (args) {
-  log.info("version", pkg.version);
-  program
-    .name(Object.keys(pkg.bin)[0])
-    .usage("<command> [options]")
-    .version(pkg.version)
-    .option("-d, --debug", "是否开启调试模式", false)
-    .hook("preAction", preAction);
-
-  // program
-  //   .command("init [name]")
-  //   .description("init project")
-  //   .option("-f, --force", "是否强制更新", false)
-  //   .action((name, opts) => {
-  //     console.log("init...", name, opts);
-  //   });
-
+  const program = createCLI();
   createInitCommand(program);
-
   program.parse(process.argv);
 }
