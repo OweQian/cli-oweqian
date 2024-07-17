ca0760822b677d3b58c373f89260a6782ffac8bc
qian wang <1510106069@qq.com>
2024-07-17 15:55:48 +0800

    feat: github源码搜索功能开发

--- packages/install/lib/index.js
+++ packages/install/lib/index.js
@@ -8,6 +8,9 @@ import {
   getGitPlatform,
 } from "@oweqian/utils";
 
+const PREV_PAGE = "${prev_page}";
+const NEXT_PAGE = "${next_page}";
+
 class InstallCommand extends Command {
   get command() {
     return "install";
@@ -53,7 +56,7 @@ class InstallCommand extends Command {
   }
   async searchGitAPI() {
     // 1、收集搜索关键词和开发语言
-    const q = await makeInput({
+    this.q = await makeInput({
       message: "请输入搜索关键词",
       validate(value) {
         if (value.length > 0) {
@@ -63,30 +66,82 @@ class InstallCommand extends Command {
         }
       },
     });
-    const language = await makeInput({
+    this.language = await makeInput({
       message: "请输入开发语言",
     });
-    log.verbose("search params", q, language, this.gitAPI.getPlatform());
-
-    const platform = this.gitAPI.getPlatform();
+    log.verbose(
+      "search keyword",
+      this.q,
+      this.language,
+      this.gitAPI.getPlatform()
+    );
     this.page = 1;
+    this.perPage = 10;
+    await this.doSearch();
+  }
+
+  async doSearch() {
+    const platform = this.gitAPI.getPlatform();
     let searchResult;
+    let count;
+    let list;
     if (platform === "github") {
       // 2、生成搜索参数
       const params = {
-        q: `${q}${language ? `+language:${language}` : ""}`,
+        q: `${this.q}${this.language ? `+language:${this.language}` : ""}`,
         order: "desc",
         sort: "stars",
-        per_page: 5,
+        per_page: this.perPage,
         page: this.page,
       };
+      log.verbose("search params", params);
       // github
-      const searchResult = await this.gitAPI.searchRepositories({
+      searchResult = await this.gitAPI.searchRepositories({
         ...params,
       });
-
-      console.log(searchResult);
+      count = searchResult.total_count; // 整体数据量
+      list = searchResult.items.map((item) => ({
+        name: `${item.full_name}${item.description}`,
+        value: item.full_name,
+      }));
     }
+
+    // 判断当前页面是否已经到达最大页面
+    if (this.page * this.perPage < count) {
+      list.push({
+        name: "下一页",
+        value: NEXT_PAGE,
+      });
+    }
+    if (this.page > 1) {
+      list.unshift({
+        name: "上一页",
+        value: PREV_PAGE,
+      });
+    }
+    const keyword = await makeList({
+      message: `请选择要下载的项目（共${count}条数据）`,
+      choices: list,
+    });
+
+    if (keyword === NEXT_PAGE) {
+      await this.nextPage();
+    } else if (keyword === PREV_PAGE) {
+      await this.prevPage();
+    } else {
+      // 下载项目
+    }
+    console.log(keyword);
+  }
+
+  async nextPage() {
+    this.page++;
+    await this.doSearch();
+  }
+
+  async prevPage() {
+    this.page--;
+    await this.doSearch();
   }
 }
 
